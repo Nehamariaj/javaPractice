032ab4aa8b3d4e42eec77fb8afdc2eb918aca75a
Nehamariaj <145547103+Nehamariaj@users.noreply.github.com>
2023-11-08 20:18:13 +0530

    Add files via upload

---
+++ SearchInAnArray.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class SearchInAnArray {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the limit:");
+        int limit=sc.nextInt();
+        int[]a=new int[limit];
+        System.out.println("enter the elements:");
+        for(int i=0;i<limit;i++){
+            a[i]=sc.nextInt();
+        }
+        Arrays.sort(a);//always sort the elements before searching
+        System.out.println("enter the element to be searched:");
+        int search=sc.nextInt();
+        int result=Arrays.binarySearch(a,search);
+        if(result>=0){
+             System.out.println(search+" is found at position "+(result+1));
+        }
+       else{
+        System.out.println(search+" is not found in the array");
+       }
+    }
+}

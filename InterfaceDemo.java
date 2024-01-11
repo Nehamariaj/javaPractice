52fcf613704c30d732aac847bce59e1b5564f883
Nehamariaj <145547103+Nehamariaj@users.noreply.github.com>
2024-01-11 13:07:24 +0530

    Add files via upload

--- InterfaceDemo.java
+++ InterfaceDemo.java
@@ -1,87 +1,59 @@
-
-abstract class Animal{
-	protected int legs;
-	protected Animal(int legs) {//constructor-if it has a same variable as that of class variable
-		this.legs=legs;//this.class variable=method argument.
-		//this is used to differentiate the class variable and argument since both have same name
-	}
-	public abstract void eat();
-	public void walk() {
-		System.out.println("this animal walks on"+legs+"legs");
-	}
+import java.util.Scanner;
+interface Shape{
+    void shapeType();
 }
-class Spider extends Animal{
-	public Spider(){//to call the constructor
-		super(8);//the value 8 is passed to walk() method
-	}
-	//override
-	public void eat() {
-		System.out.println("the spider eats insect");
-	}
+interface Polygon{
+    void getArea(int length,int breadth);
 }
-interface Pet{//in interface by default all the methods are abstract and all variables are public
-	String getName();
-	void setName(String Name);
-	void play();
+class Rectangle implements Polygon,Shape{ //a class that implements 2 interfaces
+    public void getArea(int length,int breadth){
+        System.out.println("Area="+(length*breadth));
+    }
+    public void shapeType(){
+        System.out.println("Shape:Rectangle");
+    }
+   
 }
-class Cat extends Animal implements Pet{
-	private String name;
-	public Cat() {
-		super(4);
-	}
-	//override
-	public void eat() {
-		System.out.println("The cat is eating");
-	}
-	public void setName(String name) {
-		this.name=name;
-	}
-	public String getName() {
-		return this.name;
-	}
-	public void play() {
-		System.out.println("the cat is playing");
-	}
+interface Square extends Shape{ //a interface which extends another interface
+    void sides();
 }
-class Fish extends Animal implements Pet{
-	String name;
-	public Fish() {
-		super(0);
-	}
-	public void setName(String name) {
-		this.name=name;
-	}
-	public String getName() {
-		return this.name;
-	}
-	public void play() {
-		System.out.println("the fish is playing");
-	}
-	public void walk() {
-		System.out.println("the fish cannot walk but it swims");
-	}
-	public void eat() {
-		System.out.println("the fish eats plants");
-	}
+class Square1 implements Square{
+    public void shapeType(){
+        System.out.println("Shape:Square");
+    }
+    public void sides(){
+        System.out.println("I have 4 sides");
+    }
+}
+class Triangle extends AreaOfTriangleUsingHeronsFormula implements Shape{  //class which extends a class(not defined in this class but as another class in this package) and implements a interface
+     public void shapeType(){
+        System.out.println("Shape:Triangle");
+    }
+    public void area(){
+        System.out.println("Area="+calculatingArea(3,3,3)); //reusing a method which is already defined in a different class of same package 
+    }
 }
 public class InterfaceDemo {
-          public static void main(String[] args) {
-        	 Fish fish=new Fish();
-        	 fish.setName("Mimi");
-        	 System.out.println("This fish's name is "+fish.getName());
-        	 fish.eat();
-        	 fish.walk();
-        	 fish.setName("Momo");
-        	 System.out.println("This fish's name is "+fish.getName());
-        	 Cat cat=new Cat();
-        	 cat.setName("Fluffy");
-        	 System.out.println("This cat's name is "+cat.getName());
-        	 cat.eat();
-        	 cat.walk();
-        	 cat.setName("Moose");
-        	 System.out.println("This cat's name is "+cat.getName());
-        	 Spider spider=new Spider();
-        	 spider.eat();
-        	 spider.walk();        	 
-          }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the length and breadth of rectangle:");
+        int length=sc.nextInt();
+        int breadth=sc.nextInt();
+        System.out.println();
+        //Rectangle
+        Rectangle rectangle=new Rectangle();
+        rectangle.shapeType();
+        rectangle.getArea(length, breadth);
+        System.out.println();
+        //Square
+        Square1 square1=new Square1();
+        square1.shapeType();
+        square1.sides();
+        System.out.println();
+        //Triangle
+        Triangle triangle=new Triangle();
+        triangle.shapeType();
+        triangle.area();
+        System.out.println();
+    }
 }
